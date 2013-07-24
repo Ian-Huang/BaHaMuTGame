1bed21a4547930d36a8071413afdd343430b2176
ShinMin <joey514111@hotmail.com>
2013-07-24 11:43:35 +0800

    UPDATE

--- Assets/Scripts/LoadNextScene.cs
+++ Assets/Scripts/LoadNextScene.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 using System.Collections;
 
 public class LoadNextScene : MonoBehaviour
-{   
+{
+    public bool showGUI;
     public int GUIdepth;                            //貼圖深度(值越小越後畫)
     public Texture TextureResoure;                  //貼圖素材
 
@@ -18,24 +19,33 @@ public class LoadNextScene : MonoBehaviour
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
-        iTween.ColorTo(this.gameObject, iTween.Hash("a", 0, "looptype", iTween.LoopType.pingPong));
+        //  iTween.ColorTo(this.gameObject, iTween.Hash("a", 0, "looptype", iTween.LoopType.pingPong));
     }
 
-    void OnLevelWasLoaded(int level)
+    void Update()
     {
-        Destroy(this.gameObject);        
-    }
-    
-    void OnGUI()
-    {
-        GUI.depth = this.GUIdepth;
-
         if (Application.isLoadingLevel)
         {
-            GUI.Box(new Rect(
-                0,0, Screen.width, Screen.height),
-                this.TextureResoure
-                );
+            showGUI = true;
         }
+    }
+
+    IEnumerator OnLevelWasLoaded(int level)
+    {
+        yield return new WaitForSeconds(5.0F);
+        Destroy(this.gameObject);
+    }
+
+    void OnGUI()
+    {
+        if (!showGUI) return;
+        GUI.depth = this.GUIdepth;
+
+
+        GUI.DrawTexture(new Rect(
+            0, 0, Screen.width, Screen.height),
+            this.TextureResoure
+            );
+
     }
 }
