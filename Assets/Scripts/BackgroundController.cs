16c9d0b11d9d54769ebc7f59c2e9502bd990bf27
Ian-Huang <dk781020@hotmail.com>
2013-07-30 19:31:41 +0800

    更新平原背景

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /// <summary>
 /// Create Date：2013-07-22
-/// Modify Date：2013-07-25
+/// Modify Date：2013-07-30
 /// Author：Ian
 /// Description：
 ///     背景控制器(控制背景移動、判斷背景移動的狀態)
@@ -12,7 +13,18 @@ public class BackgroundController : MonoBehaviour
 {
     public static BackgroundController script;
 
-    public float BackgroundMoveSpeed;   //背景移動的速度
+    /// <summary>
+    /// 背景資料集合
+    /// </summary>
+    [System.Serializable]
+    public class BackgroundData
+    {
+        public string Name;
+        public GameObject Background;
+        public float MoveSpeed;
+    }
+    public List<BackgroundData> BackgroundList = new List<BackgroundData>();    //背景資料集合清單
+
     public bool isRunning;              //控制是否運作
 
     void Awake()
@@ -23,8 +35,12 @@ public class BackgroundController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //判斷是否要進行移動
+        //判斷是否要移動背景
         if (this.isRunning)
-            this.transform.Translate(this.BackgroundMoveSpeed * Time.deltaTime, 0, 0);
+        {
+            //讀取背景清單內資訊，依據不同數值控制移動速度
+            foreach (var data in this.BackgroundList)
+                data.Background.transform.Translate(-data.MoveSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }
