c6dbbed549cc686738e3d7c219963db3125a198c
Ian-Huang <dk781020@hotmail.com>
2013-08-03 00:08:27 +0800

    修正 射擊物件的發射位置和爆炸中心

--- Assets/Scripts/Others/ShootObjectInfo.cs
+++ Assets/Scripts/Others/ShootObjectInfo.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 /// <summary>
 /// Create Date：2013-07-25
-/// Modify Date：2013-07-25
+/// Modify Date：2013-08-03
 /// Author：Ian
 /// Description：
 ///     遠距離(角色/敵人)發射的物件資訊
@@ -32,6 +32,7 @@ public class ShootObjectInfo : MonoBehaviour
 
                     //以下產生爆炸事件
                     this.isExplosion = true;
+                    this.boneAnimation.mLocalTransform.position = other.transform.position - new Vector3(0, 0, 0.1f);   //爆炸中心為被爆炸物中心
                     this.boneAnimation.Play("explosion");
 
                     //移除Script，使爆炸位置固定、換圖正常
