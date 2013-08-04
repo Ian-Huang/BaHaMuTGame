e8f91b31f5ee130a52c50058579b9954fa3dd4c7
ShinMin <joey514111@hotmail.com>
2013-08-04 18:30:26 +0800

    update

--- Assets/MUI/Event/Event_LayoutEnable.cs
+++ Assets/MUI/Event/Event_LayoutEnable.cs
@@ -1,22 +1,35 @@
 using UnityEngine;
 using System.Collections;
 
-public class Event_LayoutEnable : MonoBehaviour {
-
+public class Event_LayoutEnable : MonoBehaviour
+{
     public int ObjectCount;
     public Object[] LayoutObjects;
-	// Use this for initialization
-	void Start () {
+
+    public enum Type { None, Add, Dec };
+    public Type type;
+    // Use this for initialization
+    void Start()
+    {
         foreach (GameObject gameObject in LayoutObjects)
             gameObject.SetActive(false);
+
+
+        switch (type)
+        {
+            case Type.Add:
+                ObjectCount++;
+                break;
+            case Type.Dec:
+                ObjectCount--;
+                break;
+        }
         if (LayoutObjects[ObjectCount]) ((GameObject)LayoutObjects[ObjectCount]).SetActive(true);
-        
-        Destroy(this.gameObject);
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
---
+++ Assets/MUI/Event/Event_TeamMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event_TeamMenu : MonoBehaviour {
+
+    public static int ObjectCount;
+    public Object[] LayoutObjects;
+
+    public enum Type { None, Add, Dec };
+    public Type type;
+    // Use this for initialization
+    void Start()
+    {
+        foreach (GameObject gameObject in LayoutObjects)
+            gameObject.SetActive(false);
+
+
+        switch (type)
+        {
+            case Type.Add:
+                if(ObjectCount < LayoutObjects.Length - 1)
+                ObjectCount++;
+                break;
+            case Type.Dec:
+                if (ObjectCount > 0)
+                ObjectCount--;
+                break;
+        }
+        if (LayoutObjects[ObjectCount]) ((GameObject)LayoutObjects[ObjectCount]).SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
