df7a5fdd3808c90a5e879bdf0fa99eae038d24eb
Ian-Huang <dk781020@hotmail.com>
2013-08-06 18:15:52 +0800

    Update ~

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class BossController : MonoBehaviour
 {
+    public GameObject FarShootObject;
+
     private Vector3 originScale;
 
     private MoveController moveController;
     private SmoothMoves.BoneAnimation boneAnimation;
+
 
     // Use this for initialization
     void Start()
@@ -24,6 +27,38 @@ public class BossController : MonoBehaviour
     void Update()
     {
         #region TEST
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            this.boneAnimation.Play("發射");
+            Instantiate(EffectCreator.script.道路危險提示[0]);
+            //Instantiate(EffectCreator.script.道路危險提示[0]);
+            //Vector3 Posv3 = RolesCollection.script.Role1.transform.position;
+            //Posv3.x = this.transform.position.x;
+            //Instantiate(this.FarShootObject, Posv3, this.FarShootObject.transform.rotation);
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Instantiate(EffectCreator.script.道路危險提示[1]);
+            Vector3 Posv3 = RolesCollection.script.Role2.transform.position;
+            Posv3.x = this.transform.position.x;
+            Instantiate(this.FarShootObject, Posv3, this.FarShootObject.transform.rotation);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Instantiate(EffectCreator.script.道路危險提示[2]);
+            Vector3 Posv3 = RolesCollection.script.Role3.transform.position;
+            Posv3.x = this.transform.position.x;
+            Instantiate(this.FarShootObject, Posv3, this.FarShootObject.transform.rotation);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Instantiate(EffectCreator.script.道路危險提示[3]);
+            Vector3 Posv3 = RolesCollection.script.Role4.transform.position;
+            Posv3.x = this.transform.position.x;
+            Instantiate(this.FarShootObject, Posv3, this.FarShootObject.transform.rotation);
+        }
+
 
         this.transform.localScale = this.originScale;
 
@@ -57,6 +92,10 @@ public class BossController : MonoBehaviour
         {
             this.boneAnimation.Play("出現");
         }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            this.boneAnimation.Play("突刺");
+        }
         else
         {
             if (!this.animation.isPlaying)
@@ -74,11 +113,21 @@ public class BossController : MonoBehaviour
     /// <param name="triggerEvent">觸發相關資訊</param>
     public void UserTrigger(SmoothMoves.UserTriggerEvent triggerEvent)
     {
-        //確認是由"appear"觸發的UserTrigger
+        //確認是由"出現"觸發的UserTrigger
         if (triggerEvent.animationName == "出現")
         {
             //鏡頭震動
             iTween.ShakePosition(Camera.main.gameObject, new Vector3(1, 1, 0), 0.15f);
+        }
+
+        //確認是由"發射"觸發的UserTrigger
+        else if (triggerEvent.animationName == "發射")
+        {
+            //發射史萊姆砲
+            
+            Vector3 Posv3 = RolesCollection.script.Role1.transform.position;
+            Posv3.x = this.transform.position.x;
+            Instantiate(this.FarShootObject, Posv3, this.FarShootObject.transform.rotation);
         }
     }
 }
