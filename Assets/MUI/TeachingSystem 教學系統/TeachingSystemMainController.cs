daefa854a9b605d047815d758bc80d29f0d03689
ShinMin <joey514111@hotmail.com>
2013-08-11 21:01:13 +0800

    教學模式

---
+++ Assets/MUI/TeachingSystem 教學系統/TeachingSystemMainController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeachingSystemMainController : MonoBehaviour {
+
+    public bool isBackgroindRunning;
+    public bool isPlayerHalfHP;
+    public bool isRecoverCure;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnEnable()
+    {
+        if (!isBackgroindRunning)
+            BackgroundController.script.isRunning = false;
+
+        if (isPlayerHalfHP)
+        {
+           RolePropertyInfo[] rolePropertyInfo = GameObject.Find("RolesCollection").GetComponentsInChildren<RolePropertyInfo>();
+           foreach (RolePropertyInfo r in rolePropertyInfo)
+           {
+               r.currentLife = r.currentLife / 2;
+               r.cureRate = 0;
+           }
+
+        }
+
+        if (isRecoverCure)
+        {
+            RolePropertyInfo[] rolePropertyInfo = GameObject.Find("RolesCollection").GetComponentsInChildren<RolePropertyInfo>();
+            foreach (RolePropertyInfo r in rolePropertyInfo)
+            {
+                r.cureRate = 5;
+            }
+        }
+
+    }
+
+    void OnDisable()
+    {
+        BackgroundController.script.isRunning = true;
+    }
+
+}
---
+++ Assets/MUI/TeachingSystem 教學系統/TeachingSystemMovingObstacle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeachingSystemMovingObstacle : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+	}
+}
