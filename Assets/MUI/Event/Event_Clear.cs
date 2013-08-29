b7db9c827baf449144e2abe32b88e604058e5e5d
joeytsao <joey514111@hotmail.com>
2013-08-30 06:01:24 +0800

    錯誤修正 在開始畫面增加三個測試按鈕 教學系統 選關卡介面 場景加入 金幣介面加入

---
+++ Assets/MUI/Event/Event_Clear.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event_Clear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefsDictionary.PlayerPrefDictionary.Clear();
+        Application.LoadLevel("StartScene");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/MUI/Event/Event_LevelCompleteAdd.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event_LevelCompleteAdd : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefsDictionary.script.SetValue("LevelComplete", PlayerPrefsDictionary.script.GetValue("LevelComplete") + 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
