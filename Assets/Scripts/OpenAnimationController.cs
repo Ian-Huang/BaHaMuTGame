c6e73c264a1db4fad0be0d206fc24c07e9a54ccf
Ian-Huang <dk781020@hotmail.com>
2013-08-31 04:16:12 +0800

    開頭動畫音樂架構完成

--- Assets/Scripts/OpenAnimationController.cs
+++ Assets/Scripts/OpenAnimationController.cs
@@ -10,6 +10,12 @@ using System.Collections;
 public class OpenAnimationController : MonoBehaviour
 {
     private SmoothMoves.BoneAnimation boneAnimation;
+    public GameObject 城堡民宅_音效;
+    public GameObject 大史萊姆落下_音效;
+    public GameObject 小史萊姆落下_音效;
+    public GameObject 邪氣降臨_音效;
+    public GameObject 釋放邪氣_音效;
+    public GameObject 魔王城_音效;
 
     // Use this for initialization
     void Start()
@@ -25,8 +31,20 @@ public class OpenAnimationController : MonoBehaviour
     /// <param name="triggerEvent">觸發相關資訊</param>
     public void AnimationEventFunction(SmoothMoves.UserTriggerEvent triggerEvent)
     {
+        //處理音效控制
+        if (triggerEvent.boneName == "城堡民宅")
+            Instantiate(this.城堡民宅_音效);
+        else if (triggerEvent.boneName == "大史萊姆落下")
+            Instantiate(this.大史萊姆落下_音效);
+        else if (triggerEvent.boneName == "小史萊姆落下")
+            Instantiate(this.小史萊姆落下_音效);
+        else if (triggerEvent.boneName == "邪氣降臨")
+            Instantiate(this.邪氣降臨_音效);
+        else if (triggerEvent.boneName == "釋放邪氣")
+            Instantiate(this.釋放邪氣_音效);
+        else if (triggerEvent.boneName == "魔王城")
+            Instantiate(this.魔王城_音效);
 
-        //if (triggerEvent.boneName == "weapon" && triggerEvent.animationName == "attack")
         //結尾
         if (triggerEvent.normalizedTime == 1)
         {
