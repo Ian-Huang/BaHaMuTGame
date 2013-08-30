42c775a4068525884e562e97611236995bd2f728
Ian-Huang <dk781020@hotmail.com>
2013-08-31 03:47:38 +0800

    開始動畫結束後進入選關場景\

---
+++ Assets/Scripts/OpenAnimationController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Modify Date：2013-08-31
+/// Author：Ian
+/// Description：
+///     開始動畫事件控制器
+/// </summary>
+public class OpenAnimationController : MonoBehaviour
+{
+    private SmoothMoves.BoneAnimation boneAnimation;
+
+    // Use this for initialization
+    void Start()
+    {
+        //設定BoneAnimation
+        this.boneAnimation = this.GetComponent<SmoothMoves.BoneAnimation>();
+        this.boneAnimation.RegisterUserTriggerDelegate(AnimationEventFunction);
+    }
+
+    /// <summary>
+    /// 動畫事件
+    /// </summary>
+    /// <param name="triggerEvent">觸發相關資訊</param>
+    public void AnimationEventFunction(SmoothMoves.UserTriggerEvent triggerEvent)
+    {
+
+        //if (triggerEvent.boneName == "weapon" && triggerEvent.animationName == "attack")
+        //結尾
+        if (triggerEvent.normalizedTime == 1)
+        {
+            MUI_LoadSceneTransitionsEffect.script.LoadScene("LevelSelectScene", 0);
+        }
+    }
+}
